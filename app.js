7269f7fd1c44151159646c29126f84635d8b9193
Ayan Pasha <jordantoronto3@gmail.com>
2026-05-03 03:47:22 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+// Chapter 5 - Math Expressions (Familiar Operators)
+
+// Q1
+var num = 10;
+var result = num + 5;
+alert(result);
+
+// Q2
+var num1 = 20;
+var num2 = 10;
+alert(num1 - num2);
+
+// Q3
+var a = 5;
+var b = 3;
+alert(a * b);
+
+// Q4
+var x = 20;
+var y = 4;
+alert(x / y);
+
+// Q5
+var total = 10;
+total = total + 1;
+alert(total);
+
+// Q6 (increment operator)
+var num = 5;
+num++;
+alert(num);
+
+// Q7 (decrement operator)
+var num2 = 5;
+num2--;
+alert(num2);
